af3e92231a5aadc6232456b96a68d27695c9cf76
davidcalkins <david.calkins.1000@gmail.com>
2018-11-05 11:42:18 -0500

    Fixed one failing test, can't use iteration

--- src/main/java/com/jpmorgan/service/AuthenticationService.java
+++ src/main/java/com/jpmorgan/service/AuthenticationService.java
@@ -2,6 +2,7 @@ package com.jpmorgan.service;
 
 import java.time.ZonedDateTime;
 import java.util.Iterator;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 import org.springframework.stereotype.Service;
@@ -15,8 +16,11 @@ public class AuthenticationService {
 	
 	/** A singleton, ordered set that provides concurrent access. It is 
 	 * sorted based on the expiration. */
-	static ConcurrentSkipListSet<UserToken> sessions = new ConcurrentSkipListSet<>(
+	static ConcurrentSkipListSet<UserToken> sessionsByTime = new ConcurrentSkipListSet<>(
 			(UserToken a, UserToken b) -> a.getInvalidateTime().compareTo(b.getInvalidateTime()));
+	
+	static ConcurrentSkipListMap<UserToken,UserToken> sessionsByToken = new ConcurrentSkipListMap<>(
+			(UserToken a, UserToken b) -> a.getUserTokenStr().compareTo(b.getUserTokenStr()));
 	
 	static Object lock = new Object();
 	
@@ -36,7 +40,10 @@ public class AuthenticationService {
 							userCredentials.getUsername() + userCredentials.getPassword()), 
 					ZonedDateTime.now().plusSeconds(EXPIRATION_SECONDS));
 			
-			sessions.add(userToken);
+			synchronized(lock) {
+				sessionsByTime.add(userToken);
+				sessionsByToken.put(userToken,userToken);
+			}
 			
 			return userToken;
 		}
@@ -45,14 +52,12 @@ public class AuthenticationService {
 	}
 	
 	public void logout(UserToken userToken) {
-		for (Iterator<UserToken> it = sessions.iterator(); it.hasNext();) {
-			UserToken tempToken = it.next();
-			
-			if (tempToken.getUserTokenStr().equals(userToken.getUserTokenStr())) {
-				it.remove();
-//				tempToken.setInvalidateTime(ZonedDateTime.now().minusDays(1));
-				
-				break;
+		UserToken actualToken = sessionsByToken.get(userToken);
+		
+		if (actualToken!= null) {
+			synchronized(lock) {
+				sessionsByToken.remove(actualToken);
+				sessionsByTime.remove(actualToken);
 			}
 		}
 	}
@@ -60,17 +65,21 @@ public class AuthenticationService {
 	public boolean isValid(UserToken userToken) {
 		removeExpiredSessions();
 		
-		System.out.println("Sessions: " + sessions);
-		System.out.println("Is Valid?: " + userToken);
-		System.out.println("IsValid: " + sessions.contains(userToken));
+//		System.out.println("Sessions: " + sessions);
+//		System.out.println("Is Valid?: " + userToken);
+//		System.out.println("IsValid: " + sessions.contains(userToken));
+//		System.out.print("Actual: ");
 		
-		for (Iterator<UserToken> it = sessions.iterator(); it.hasNext();) {
-			UserToken tempToken = it.next();
-			if (tempToken.getUserTokenStr().equals(userToken.getUserTokenStr())) {
-				return true;
-			}
-		}
-		return false;
+//		for (Iterator<UserToken> it = sessions.iterator(); it.hasNext();) {
+//			UserToken tempToken = it.next();
+//			if (tempToken.getUserTokenStr().equals(userToken.getUserTokenStr())) {
+//				System.out.println(true);
+//				return true;
+//			}
+//		}
+//		System.out.println(false);
+//		return false;
+		return sessionsByToken.containsKey(userToken);
 	}
 	
 	/**
@@ -81,22 +90,26 @@ public class AuthenticationService {
 		
 		UserToken session;
 		
+		synchronized(lock) {
 		// Remove sessions while there are sessions that 
-		while (!sessions.isEmpty() && sessions.first().getInvalidateTime().compareTo(currentTime) < 0) {
-			session = sessions.pollFirst();
-			// Due to concurrent access, more than one element might be removed. 
-			// To handle this, if an element is removed that hasn't expired, it 
-			// is put back. 
-			if (session.getInvalidateTime().compareTo(currentTime) >= 0) {
-				sessions.add(session);
-			}
+		while (!sessionsByTime.isEmpty() && sessionsByTime.first().getInvalidateTime().compareTo(currentTime) < 0) {
+			session = sessionsByTime.pollFirst();
+			sessionsByToken.remove(session);
+//			// Due to concurrent access, more than one element might be removed. 
+//			// To handle this, if an element is removed that hasn't expired, it 
+//			// is put back. 
+//			if (session.getInvalidateTime().compareTo(currentTime) >= 0) {
+//				sessions.add(session);
+//			}
+		}
 		}
 	}
 	
 	/** Removes all sessions from the service. Primarily for testing. */
 	public void logoutAll() {
 		System.out.println("Called logoutAll. Should only for testing. ");
-		sessions.clear();
+		sessionsByTime.clear();
+		sessionsByToken.clear();
 	}
 	
 	/** Sets the time until a new session expires in seconds. */
