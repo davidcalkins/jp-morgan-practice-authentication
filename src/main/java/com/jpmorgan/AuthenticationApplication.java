f067984d3ffeaa6c9daa5868e663824166393e6c
davidcalkins <david.calkins.1000@gmail.com>
2018-11-09 11:48:17 -0500

    Now sessions are backed by the database

--- src/main/java/com/jpmorgan/AuthenticationApplication.java
+++ src/main/java/com/jpmorgan/AuthenticationApplication.java
@@ -3,9 +3,11 @@ package com.jpmorgan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaRepositories
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
